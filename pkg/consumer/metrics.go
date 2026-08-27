d09a7046bf1f0b1c2efd1442c9ba12e5a654bec9
Nus <200475008+nus25@users.noreply.github.com>
2026-08-27 10:23:09 +0000

    delete unused metrics

--- pkg/consumer/metrics.go
+++ pkg/consumer/metrics.go
@@ -5,42 +5,6 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
-var eventsProcessedCounter = promauto.NewCounterVec(prometheus.CounterOpts{
-	Name: "consumer_events_processed_total",
-	Help: "The total number of firehose events processed by Consumer",
-}, []string{"event_type", "socket_url"})
-
-var opsProcessedCounter = promauto.NewCounterVec(prometheus.CounterOpts{
-	Name: "consumer_ops_processed_total",
-	Help: "The total number of repo operations processed by Consumer",
-}, []string{"kind", "op_path", "socket_url"})
-
-var eventProcessingDurationHistogram = promauto.NewHistogramVec(prometheus.HistogramOpts{
-	Name:    "consumer_event_processing_duration_seconds",
-	Help:    "The amount of time it takes to process a firehose event",
-	Buckets: prometheus.ExponentialBuckets(0.0001, 2, 18),
-}, []string{"socket_url"})
-
-var lastSeqGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
-	Name: "consumer_last_seq",
-	Help: "The sequence number of the last event processed",
-}, []string{"socket_url"})
-
-var lastEvtProcessedAtGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
-	Name: "consumer_last_evt_processed_at",
-	Help: "The timestamp of the last event processed",
-}, []string{"socket_url"})
-
-var lastEvtCreatedAtGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
-	Name: "consumer_last_evt_created_at",
-	Help: "The timestamp of the last event created",
-}, []string{"socket_url"})
-
-var lastEvtCreatedEvtProcessedGapGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
-	Name: "consumer_last_evt_created_evt_processed_gap",
-	Help: "The gap between the last event's event timestamp and when it was processed by consumer",
-}, []string{"socket_url"})
-
 var eventsSequencedCounter = promauto.NewCounterVec(prometheus.CounterOpts{
 	Name: "consumer_events_sequenced_total",
 	Help: "The total number of events sequenced",
